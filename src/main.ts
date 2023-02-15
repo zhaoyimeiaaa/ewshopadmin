8b6dbe5b14cbcdf9a453695c521f3e87b826a19e
xinru <xinru@xinru.com>
2023-02-15 17:09:25 +0800

    '抽取路由模块配置默认端口’

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from "./router";
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+
+// 5. 创建并挂载根实例
+app.use(router)
+app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import {NaiveUiResolver} from "unplugin-vue-components/resolvers";
+import { resolve} from 'path';
+// import * as process from "process";
+
+
+function pathResolve(dir: string) {
+    // 获取绝对定位的方法
+    return resolve(process.cwd(),'.',dir);
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+      Components({
+        resolvers:[NaiveUiResolver()]
+      })
+  ],
+    resolve:{
+      alias:[
+          {
+              // 简化路径前缀，使用绝对地址
+              find:'@',
+              replacement:(pathResolve('src'))
+          }
+      ]
+    },
+    server: {
+        host: 'localhost',
+        port: 8000
+    }
 })
