fb4ec2574894a9603cc78f9b9462c7f60c0cad29
xinru <xinru@xinru.com>
2023-02-17 18:36:34 +0800

    '配置路由跳转'

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
 import {createRouter, createWebHistory} from "vue-router";
 import Home from '@/views/home/Home.vue'
-import Login from '@/views/login/index.vue'
 import index from '@/views/login/index.vue'
 import Dashboard from '@/views/dashboard/Dashboard.vue';
+import dashboard from "@/router/modules/dashboard";
 
 
 // 2. 定义一些路由
@@ -15,13 +15,15 @@ const routes = [
     // {path: '/index', component: index},
 ]
 
+const baseRoutes = [...routes, ...dashboard];
+
 // 3. 创建路由实例并传递 `routes` 配置
 // 你可以在这里输入更多的配置，但我们在这里
 // 暂时保持简单
 const router = createRouter({
     // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
     history: createWebHistory(),
-    routes, // `routes: routes` 的缩写
+    routes:baseRoutes, // `routes: routes` 的缩写
 })
 
 router.beforeEach((to, from, next) => {
